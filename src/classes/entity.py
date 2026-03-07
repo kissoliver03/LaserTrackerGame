2bd1e06f67a57333f5d0a500d07c8fee5ecb0d57
kissoliver03 <kissoliver03@gmail.com>
2026-03-07 13:08:01 +0100

    Made entity.py Basic class loader

---
+++ src/classes/entity.py
@@ -0,0 +1,62 @@
+import os
+
+import pygame
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, data_dict, cell_w, cell_h):
+        super().__init__()
+
+        self.WHITE = (255, 255, 255)
+
+        self.name = data_dict.get('name', 'unknown')
+        self.group = data_dict.get('group', self.name)
+        self.type = data_dict.get('type', 'static')
+        self.shape = data_dict.get('shape', 'rect')
+
+        grid_pos = data_dict.get('grid_pos', [0, 0])
+        grid_size = data_dict.get('grid_size', [1, 1])
+
+        width = int(grid_size[0] * cell_w)
+        height = int(grid_size[1] * cell_h)
+        x_pos = int(grid_pos[0] * cell_w)
+        y_pos = int(grid_pos[1] * cell_h)
+
+        model_path = data_dict.get('model', None)
+        is_image_loaded = False
+
+        if model_path:
+            abs_model_path = os.path.abspath(model_path)
+
+            try:
+                model = pygame.image.load(abs_model_path).convert_alpha()
+
+                self.image = pygame.transform.smoothscale(model, (width, height))
+                is_image_loaded = True
+
+            except FileNotFoundError:
+                is_image_loaded = False
+
+            except pygame.error as exc:
+                is_image_loaded = False
+
+        if not is_image_loaded:
+            self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+
+            if self.shape == 'rect':
+                pygame.draw.rect(self.image, self.WHITE, [x_pos, y_pos, width, height])
+            elif self.shape == 'circle':
+                radius = int(width / 2)
+                pygame.draw.circle(self.image, self.WHITE, [x_pos, y_pos], radius)
+
+        self.rect = self.image.get_rect(topleft=(x_pos, y_pos))
+
+        velocity = data_dict.get('velocity', [0, 0])
+        self.vel_x = velocity[0]
+        self.vel_y = velocity[1]
+
+    def update(self):
+        if self.type in ['dynamic', 'kinematic']:
+            self.rect.x += self.vel_x
+            self.rect.y += self.vel_y
+
+
--- src/classes/gameloader.py
+++ src/classes/gameloader.py
@@ -28,3 +28,40 @@ class GameLoader:
         except Exception as exc:
             self.is_level_loaded = False
             return False
+
+
+    def get_meta(self):
+        if self.is_level_loaded:
+            return self.data.get('meta', {})
+        else:
+            return {}
+
+    def get_globals(self):
+        if self.is_level_loaded:
+            return self.data.get('globals', {})
+        else:
+            return {}
+
+    def get_layout(self):
+        if self.is_level_loaded:
+            return self.data.get('layout', {})
+        else:
+            return {}
+
+    def get_entities(self):
+        if self.is_level_loaded:
+            return self.data.get('entities', [])
+        else:
+            return []
+
+    def get_inputs(self):
+        if self.is_level_loaded:
+            return self.data.get('inputs', [])
+        else:
+            return []
+
+    def get_rules(self):
+        if self.is_level_loaded:
+            return self.data.get('rules', [])
+        else:
+            return []
