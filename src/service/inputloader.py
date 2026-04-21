7b2d7e6f316dad0a30828b6304a4d654f7f04aa7
kissoliver03 <kissoliver03@gmail.com>
2026-04-21 17:33:30 +0200

    feat: created inputloader.py

---
+++ src/service/inputloader.py
@@ -0,0 +1,30 @@
+from pygrabber.dshow_graph import FilterGraph
+
+class InputLoader:
+    def __init__(self):
+        self.available_cams = self.detect_cameras()
+
+    def detect_cameras(self):
+        try:
+            graph = FilterGraph()
+            cameras = graph.get_input_device()
+
+            if not cameras:
+                return 0
+            return cameras
+
+        except Exception as e:
+            return 0
+
+
+    def get_camera_name(self, index):
+        if self.available_cams == 0:
+            return 0
+
+        if 0 <= index < len(self.available_cams):
+            return self.available_cams[index]
+
+        return 0
+
+    def get_camera_count(self):
+        return len(self.available_cams)
